d21ffef2662e715a403a88111487a0bb98e14f03
aahmedfahmii <ae2200926@tkh.edu.eg>
2024-12-11 04:31:43 +0200

    addedmy login route

--- server.js
+++ server.js
@@ -7,3 +7,26 @@ const server =express()
 const port = 101
 server.use(cors())
 server.use(express.json())
+
+server.post('/user/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    db.get(`SELECT * FROM USERS WHERE EMAIL = ?`, [email], (err, user) => {
+      if (err) {
+        return res.status(500).send('Error on user lookup.');
+      }
+      if (!user) {
+        return res.status(401).send('User not found.');
+      }
+      bcrypt.compare(password, user.PASSWORD, (err, isMatch) => {
+        if (err) {
+          return res.status(500).send('Error comparing password.');
+        }
+        if (!isMatch) {
+          return res.status(401).send('Invalid credentials');
+        }
+        res.cookie('username', user.NAME, { httpOnly: true, maxAge: 3600000 });
+        return res.status(200).send('Login successful');
+      });
+    });
+  });
